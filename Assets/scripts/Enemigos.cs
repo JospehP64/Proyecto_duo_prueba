fc8f987d1b55ad6e17a3fb23c266d3cbbb7f9502
JospehP64 <jmjakepardo@gmail.com>
2024-11-18 12:16:06 +0100

    18-11-2024
    -Se ha creado un script para los enemigos.
    
    -Ahora los enemigos pueden caminar hacia el jugador y perseguirlo (sin embargo, los enemigos se quedan quietos cuando el jugador se pone a la izquierda de los enemigos).
    
    -Se han creado variables como la "velocidad" y la "vida" para los enemigos.

--- Assets/scripts/Enemigos.cs
+++ Assets/scripts/Enemigos.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Enemigos : MonoBehaviour
 {
+    [SerializeField]Transform Posicionjugador;
+    Rigidbody rb;
+    float vidaEnemigo;//es como la resistencia del enemigo
+    float velocidadEnemigo = 0.5f;
+    float ataqueEnemigo;
+    float rangoDeAlcance;
+    float angulo;
+    Vector3 movimiento;
+    Vector3 direccion;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        direccion = Posicionjugador.position - transform.position;
+        angulo = Mathf.Atan2(direccion.x, direccion.z) * Mathf.Rad2Deg;
+        
+        movimiento = direccion;
+        
+    }
+    private void FixedUpdate()
+    {
+        movimientoEnemigo();
+    }
+
+    void movimientoEnemigo()
+    {
+        if(angulo >= 0.10f)
+        {
+            rb.MovePosition(transform.position + (direccion * velocidadEnemigo * Time.deltaTime));
+        }
+        else if(angulo <= 0.5f)
+        {
+
+        }
         
     }
 }
